821962cf387ced80246e37863a833a834dc27420
DefectiveMan <pelaet@rpi.edu>
2017-10-07 21:51:44 -0400

    World Map can be scrolled back and forth (not infinite :(

--- js/preload.js
+++ js/preload.js
@@ -5,9 +5,11 @@ let preloadState = function(){
 
 preloadState.prototype.preload = function() {
     game.load.image("platform", "assets/platform.png");
-    game.load.image("sky", "assets/sky.png");
+    //game.load.image("sky", "assets/sky.png");
     game.load.image("star", "assets/star.png");
+    game.load.image("worldmap", "assets/BIGWORLDMAP.png");
     game.load.spritesheet("duck", "assets/dude.png", 32, 48);
+
 }
 
 preloadState.prototype.create = function() {
